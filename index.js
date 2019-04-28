20f962216938106880a75dea7f8d23ab29067947
Johnny <john.arena.3@gmail.com>
2019-04-28 12:37:33 -0500

    cleanup

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
 "use strict"
 
 const data = {
-  backgrounds: require('lib/backgrounds.json'),
-  core_bonuses: require('lib/core_bonuses.json'),
-  frames: require('lib/frames.json'),
-  info: require('lib/info.json'),
-  manufacturers: require('lib/manufacturers.json'),
-  mods: require('lib/mods.json'),
-  pilot_gear: require('lib/pilot_gear.json'),
-  quirks: require('lib/quirks.json'),
-  statuses: require('lib/statuses.json'),
-  systems: require('lib/systems.json'),
-  tags: require('lib/tags.json'),
-  talents: require('lib/talents.json'),
-  translation_table: require('lib/translation_table.json'),
-  weapons: require('lib/weapons.json'),
+  backgrounds: require('./lib/backgrounds.json'),
+  core_bonuses: require('./lib/core_bonuses.json'),
+  frames: require('./lib/frames.json'),
+  info: require('./lib/info.json'),
+  manufacturers: require('./lib/manufacturers.json'),
+  mods: require('./lib/mods.json'),
+  pilot_gear: require('./lib/pilot_gear.json'),
+  quirks: require('./lib/quirks.json'),
+  statuses: require('./lib/statuses.json'),
+  systems: require('./lib/systems.json'),
+  tags: require('./lib/tags.json'),
+  talents: require('./lib/talents.json'),
+  translation_table: require('./lib/translation_table.json'),
+  weapons: require('./lib/weapons.json'),
 }
 
 let licenses = []
-state.Frames.filter((x) => x.source.toUpperCase() !== 'GMS').forEach((frame) => {
+data.frames.filter(x => x.source.toUpperCase() !== 'GMS').forEach((frame) => {
   licenses.push({
     source: frame.source.toUpperCase(),
     license: frame.name.toUpperCase(),
@@ -30,8 +30,8 @@ state.Frames.filter((x) => x.source.toUpperCase() !== 'GMS').forEach((frame) => 
   })
 })
 let items = JSON.parse(JSON.stringify(data.weapons))
-items = items.concat(data.WeaponMods, data.MechSystems)
-items.filter((x) => x.source && x.source.toUpperCase() !== 'GMS' 
+items = items.concat(data.mods, data.systems)
+items.filter(x => x.source && x.source.toUpperCase() !== 'GMS'
   && x.source.toUpperCase() !== '').forEach((item) => {
     const idx = licenses.findIndex((x) => x.license === item.license.toUpperCase())
     licenses[idx].unlocks[item.license_level - 1].push(item)
